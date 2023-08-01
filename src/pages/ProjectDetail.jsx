23f821adce8e0b83f77c541d61aa35933eeb302a
mehmet <keskinm90@gmail.com>
2023-08-02 01:01:54 +0300

    add header and footer to project page

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -1,31 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header/Header';
+import CallToAction from '../components/CTA/CallToAction';
+import Footer from '../components/Footer/Footer';
 
 const ProjectDetail = ({ clickedProject }) => {
   const images = clickedProject.project_images;
   const types = clickedProject.project_types;
 
   return (
-    <div className="p-4">
-      <Link to={'/'} className="bg-slate-200 p-3 rounded font-bold">
-        {'<'} Back to home
-      </Link>
-      <h1 className="my-5 text-5xl font-heading font-semibold">
-        {clickedProject.project_name}
-      </h1>
-      <div className="flex gap-4 mb-5">
-        {types.map((type, index) => (
-          <span
-            className="bg-primary bg-opacity-20 px-3 py-1 rounded"
-            key={index}
-          >
-            {type}
-          </span>
+    <div className="bg-black p-24">
+      <Header />
+      <div className="mt-16">
+        <Link to={'/'} className="bg-slate-200 p-3 rounded font-bold">
+          {'<'} Back to home
+        </Link>
+        <h1 className="my-5 text-5xl font-heading font-semibold">
+          {clickedProject.project_name}
+        </h1>
+        <div className="flex gap-4 mb-5">
+          {types.map((type, index) => (
+            <span
+              className="bg-primary bg-opacity-20 px-3 py-1 rounded"
+              key={index}
+            >
+              {type}
+            </span>
+          ))}
+        </div>
+        {images.map((image, index) => (
+          <img className="mb-4" key={index} src={image} alt="" />
         ))}
       </div>
-      {images.map((image, index) => (
-        <img className="mb-4" key={index} src={image} alt="" />
-      ))}
+      <CallToAction />
+      <Footer />
     </div>
   );
 };
