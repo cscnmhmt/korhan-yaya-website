c6314d77561de4385b058e2f5efc5d0fcaf25dd3
mehmet <keskinm90@gmail.com>
2023-08-02 01:09:36 +0300

    style project detail page

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -9,27 +9,31 @@ const ProjectDetail = ({ clickedProject }) => {
   const types = clickedProject.project_types;
 
   return (
-    <div className="bg-black p-24">
+    <div className="bg-black pt-28">
       <Header />
-      <div className="mt-16">
-        <Link to={'/'} className="bg-slate-200 p-3 rounded font-bold">
-          {'<'} Back to home
-        </Link>
-        <h1 className="my-5 text-5xl font-heading font-semibold">
-          {clickedProject.project_name}
-        </h1>
-        <div className="flex gap-4 mb-5">
-          {types.map((type, index) => (
-            <span
-              className="bg-primary bg-opacity-20 px-3 py-1 rounded"
-              key={index}
-            >
-              {type}
-            </span>
-          ))}
+      <div className="border-t border-[#1f1f1f] mt-8">
+        <div className="flex justify-between items-center max-w-7xl mx-auto">
+          <h1 className="my-5 text-5xl text-white font-heading font-semibold">
+            {clickedProject.project_name}
+          </h1>
+          <div className="flex gap-4">
+            {types.map((type, index) => (
+              <span
+                className="bg-primary bg-opacity-20 px-3 py-1 rounded text-yellow-400"
+                key={index}
+              >
+                {type}
+              </span>
+            ))}
+          </div>
         </div>
         {images.map((image, index) => (
-          <img className="mb-4" key={index} src={image} alt="" />
+          <img
+            className="max-w-7xl mx-auto mb-16 rounded-md"
+            key={index}
+            src={image}
+            alt=""
+          />
         ))}
       </div>
       <CallToAction />
